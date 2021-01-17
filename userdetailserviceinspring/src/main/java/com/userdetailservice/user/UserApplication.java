c5603ba7e034c58a0b21c4f1df3e5373918b6b1c
Prateek Kumar <59215581+Enforc3rr@users.noreply.github.com>
2021-01-17 20:15:15 +0530

    Add files via upload

---
+++ userdetailserviceinspring/src/main/java/com/userdetailservice/user/UserApplication.java
@@ -0,0 +1,16 @@
+package com.userdetailservice.user;
+
+import com.userdetailservice.user.dao.UserRepo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepo.class)
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
